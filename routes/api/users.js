006a5124a3d3f82158e9e71b8d9846f7e72117a8
Akhil Chaudhary <chaudharyakhilbit@gmail.com>
2019-04-28 20:54:46 +0530

    validation handlers two added

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 //Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 //Load User model
 const User = require('../../models/User');
@@ -62,13 +63,21 @@ router.post('/register', (req, res) => {
 //#desc Login user / Returning JWT Token
 //@access Public
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  //check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
   //Find user by email
   User.findOne({ email }).then(user => {
     if (!user) {
-      return res.status(404).json({ email: 'User not found' });
+      errors.email = 'User not found';
+      return res.status(404).json(errors);
     }
     //Check password
     bcrypt.compare(password, user.password).then(isMatch => {
@@ -90,7 +99,8 @@ router.post('/login', (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: 'Password incorrect' });
+        errors.password = 'Password incorrect';
+        return res.status(400).json(errors);
       }
     });
   });
@@ -110,5 +120,4 @@ router.get(
     });
   }
 );
-
 module.exports = router;
